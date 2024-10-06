0b13d30eed985b59f2a8b0b3f68ec5830de9dddc
lawtrafalgar02 <46304349+LehrerB@users.noreply.github.com>
2024-10-06 19:15:58 +0200

    4 cubes turning

---
+++ misc/4cubescript.js
@@ -0,0 +1,535 @@
+let cubeSRC = "https://ruwix.com/widget/3d/?label=Sample&alg=F&setupmoves=B%20U2%20F%20R2%20F2%20U2%20F'%20D2%20U2%20L2%20B'%20L'%20B'%20F'%20R%20B2%20R%20U'%20L%20D&hover=1&speed=700&flags=showalg";
+//let cubeFrame = document.getElementById('cubeframe')
+let cube1 = document.getElementById('cube1');
+
+//let randAlg = "R2 B' L2 B' R2 U2 B' F2 R2 F' D L2 U R' B' D2 U' L B' D' R2' L2";
+
+let randAlgArray = [
+    "R2' D2' U B2' D U2' R2' D2' F' L' D' R D R' U2' L' F U' L' B2'",
+    "R2' D2' U B2' D U2' R2' D2' F' L' D' R D R' U2' L' F U' L' B2'",
+    "R2' D2' U B2' D U2' R2' D2' F' L' D' R D R' U2' L' F U' L' B2'",
+    "R2' D2' U B2' D U2' R2' D2' F' L' D' R D R' U2' L' F U' L' B2'",
+]
+
+let algoArray1 = [
+    ['D*/e U*/m',"F' F' F U F' y' x x' U' F' F' y' F' F' F U' R"],
+    ['D*/e */m',"U U F' F' y U F' F' y F' F' y U F' F' x x'"],
+    ['RBD U*/m',"y'"],
+    ['RBD B*/m R*/m U*/m',"y U' R U R' U'"],
+    ["BLD B*/m L*/m U*/m","y' y' y' U y L' U' L U"],
+    ["FRD F*/m R*/m","y' y y U' U' R U R' U' R U R' U' R U R' U'"],
+    ["FLD","y' R U R' U'"],
+    ["FLD F*/m L*/m","R U R' U'"],
+    ["BL","y'"],
+    ["BL B*/m L*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["RB B*/m R*/m D*","y' U U R U R' U' y L' U' L U"],
+    ["RF FLD","y R U R' U' y L' U' L U"], //11
+    ["RF R*/m F*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["FL L*/m F*/m D*","y' y U' L' U' L U y' R U R' U "],
+    ["U*/e U*/m","U f R U R' U' f'"],
+    ["R*/m F*/m B*/m L*/m U*/e U*/m","y y y U' y y y U' y' y' R U R' U R U2' R' U y y y y"],
+    ["*/m U*/c U*/e","y' y' y'"], //16
+    ["R*/m F*/m UFR","y R U' L' U R' U' L U"],
+    ["U* */m","y' y' x x R U R' U' R U R' U' x x' D R U R' U' R U R' U' x x' D D R U R' U' R U R' U' D"],
+    ["",""],
+]
+
+let algoArray2 = [
+    ['D*/e U*/m',"F' F' F U F' y' x x' U' F' F' y' F' F' F U' R"],
+    ['D*/e */m',"U U F' F' y U F' F' y F' F' y U F' F' x x'"],
+    ['RBD U*/m',"y'"],
+    ['RBD B*/m R*/m U*/m',"y U' R U R' U'"],
+    ["BLD B*/m L*/m U*/m","y' y' y' U y L' U' L U"],
+    ["FRD F*/m R*/m","y' y y U' U' R U R' U' R U R' U' R U R' U'"],
+    ["FLD","y' R U R' U'"],
+    ["FLD F*/m L*/m","R U R' U'"],
+    ["BL","y'"],
+    ["BL B*/m L*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["RB B*/m R*/m D*","y' U U R U R' U' y L' U' L U"],
+    ["RF FLD","y R U R' U' y L' U' L U"], //11
+    ["RF R*/m F*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["FL L*/m F*/m D*","y' y U' L' U' L U y' R U R' U "],
+    ["U*/e U*/m","U f R U R' U' f'"],
+    ["R*/m F*/m B*/m L*/m U*/e U*/m","y y y U' y y y U' y' y' R U R' U R U2' R' U y y y y"],
+    ["*/m U*/c U*/e","y' y' y'"], //16
+    ["R*/m F*/m UFR","y R U' L' U R' U' L U"],
+    ["U* */m","y' y' x x R U R' U' R U R' U' x x' D R U R' U' R U R' U' x x' D D R U R' U' R U R' U' D"],
+    ["",""],
+]
+
+let algoArray3 = [
+    ['D*/e U*/m',"F' F' F U F' y' x x' U' F' F' y' F' F' F U' R"],
+    ['D*/e */m',"U U F' F' y U F' F' y F' F' y U F' F' x x'"],
+    ['RBD U*/m',"y'"],
+    ['RBD B*/m R*/m U*/m',"y U' R U R' U'"],
+    ["BLD B*/m L*/m U*/m","y' y' y' U y L' U' L U"],
+    ["FRD F*/m R*/m","y' y y U' U' R U R' U' R U R' U' R U R' U'"],
+    ["FLD","y' R U R' U'"],
+    ["FLD F*/m L*/m","R U R' U'"],
+    ["BL","y'"],
+    ["BL B*/m L*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["RB B*/m R*/m D*","y' U U R U R' U' y L' U' L U"],
+    ["RF FLD","y R U R' U' y L' U' L U"], //11
+    ["RF R*/m F*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["FL L*/m F*/m D*","y' y U' L' U' L U y' R U R' U "],
+    ["U*/e U*/m","U f R U R' U' f'"],
+    ["R*/m F*/m B*/m L*/m U*/e U*/m","y y y U' y y y U' y' y' R U R' U R U2' R' U y y y y"],
+    ["*/m U*/c U*/e","y' y' y'"], //16
+    ["R*/m F*/m UFR","y R U' L' U R' U' L U"],
+    ["U* */m","y' y' x x R U R' U' R U R' U' x x' D R U R' U' R U R' U' x x' D D R U R' U' R U R' U' D"],
+    ["",""],
+]
+
+let algoArray4 = [
+    ['D*/e U*/m',"F' F' F U F' y' x x' U' F' F' y' F' F' F U' R"],
+    ['D*/e */m',"U U F' F' y U F' F' y F' F' y U F' F' x x'"],
+    ['RBD U*/m',"y'"],
+    ['RBD B*/m R*/m U*/m',"y U' R U R' U'"],
+    ["BLD B*/m L*/m U*/m","y' y' y' U y L' U' L U"],
+    ["FRD F*/m R*/m","y' y y U' U' R U R' U' R U R' U' R U R' U'"],
+    ["FLD","y' R U R' U'"],
+    ["FLD F*/m L*/m","R U R' U'"],
+    ["BL","y'"],
+    ["BL B*/m L*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["RB B*/m R*/m D*","y' U U R U R' U' y L' U' L U"],
+    ["RF FLD","y R U R' U' y L' U' L U"], //11
+    ["RF R*/m F*/m D*","U' y U' L' U' L U y' R U R' U'"],
+    ["FL L*/m F*/m D*","y' y U' L' U' L U y' R U R' U "],
+    ["U*/e U*/m","U f R U R' U' f'"],
+    ["R*/m F*/m B*/m L*/m U*/e U*/m","y y y U' y y y U' y' y' R U R' U R U2' R' U y y y y"],
+    ["*/m U*/c U*/e","y' y' y'"], //16
+    ["R*/m F*/m UFR","y R U' L' U R' U' L U"],
+    ["U* */m","y' y' x x R U R' U' R U R' U' x x' D R U R' U' R U R' U' x x' D D R U R' U' R U R' U' D"],
+    ["",""],
+]
+
+let algoArrayPrime = [algoArray1,algoArray2,algoArray3,algoArray4];
+let buttonTracker = [[],[],[],[]];
+
+let partialSolve = '';
+let startAlg = [randAlgArray[0],partialSolve].join(' ').trim();
+let testAlg = "L2";
+
+function createcubeSRC(alg,setupmoves,speed=400,sampletxt='sample',highlight='') {
+    //setupmoves = replaceXYZRUF(setupmoves);
+
+    let string = `https://ruwix.com/widget/3d/?label=${sampletxt}`;
+    string += `&alg=${alg.replaceAll(' ','%20')}`
+    string += `&setupmoves=${setupmoves.replaceAll(' ','%20')}`
+    string += `&hover=1&speed=${speed}&flags=""`; //showalg
+    if(highlight!='') {
+        string += `&colored=${highlight}`;
+    }
+    return string
+}
+
+function createConfig(alg,setupmoves,speed=400,sampletxt='sample',highlight='') {
+    //setupmoves = replaceXYZRUF(setupmoves);
+
+    let string = ``;
+    string += `alg=${alg}`
+    string += `|setupmoves=${setupmoves}`
+    string += `|hover=1|speed=${speed}|flags=''`; //showalg
+    //string += `|colors=U:#ffffff`
+    if(highlight!='') {
+        string += `|colored=${highlight}`;
+    }
+    return string
+}
+
+function initiateAlg(arrayIndex=0,alg,startAlgorithm = "", speed=400,num=0,highlightBoolean=true) {
+    let preventXYZstring = preventXYZ((countXYZ(randAlgArray[arrayIndex] + " " + alg)));
+    let startSequence = startAlgorithm == "" ? preventXYZstring + alg : preventXYZstring + startAlgorithm + " " + alg;
+    let highlight = highlightBoolean ? algoArrayPrime[arrayIndex][num][0] : "";
+    return createConfig(alg,startSequence,speed,num,highlight);
+}
+
+function getStartAlg(arrayIndex=0) {
+    return [randAlgArray[arrayIndex],partialSolve].join(' ').trim();
+}
+
+function loadCurrent(arrayIndex=0,speed=400,num,multipleBoolean=false) {
+    testAlg = algoArrayPrime[arrayIndex][[currentNumArray[arrayIndex]]][1];
+    //cubeFrame.src = initiateAlg(testAlg,getStartAlg(),speed,num);
+    clearAllCanvases();
+    removeDivById('cube'+arrayIndex+'a');
+    CubeAnimation.create_in_dom(`#parent${arrayIndex}`, initiateAlg(arrayIndex,testAlg,getStartAlg(arrayIndex),speed,num), `id=${'cube'+arrayIndex+'a'} class='cube roofpig mycube noBorder'`);
+    let cube = document.getElementById('cube'+arrayIndex+'a');
+    cube.setAttribute('data-config',initiateAlg(arrayIndex,testAlg,getStartAlg(arrayIndex),speed,num));
+    if(multipleBoolean) {
+        let highlightBoolean = false;
+        removeDivById('cube'+arrayIndex+'b');
+        CubeAnimation.create_in_dom(`#parent${arrayIndex}`, initiateAlg(arrayIndex,testAlg,getStartAlg(arrayIndex),speed,num,highlightBoolean), `id=${'cube'+arrayIndex+'b'} class='cube transparent roofpig mycube noBorder'`);
+        cube = document.getElementById('cube'+arrayIndex+'b')
+        cube.setAttribute('data-config',initiateAlg(arrayIndex,testAlg,getStartAlg(arrayIndex),speed,num,highlightBoolean));  
+    }
+    updateButtonTracker(arrayIndex);
+}
+
+function removeElementAndChildren(element) {
+    // Check if the element exists
+    if (element) {
+        // Recursively remove all children
+        while (element.firstChild) {
+            removeElementAndChildren(element.firstChild);  // Recursively remove child elements
+        }
+        // Once all children are removed, remove the parent element itself
+        element.remove();
+        console.log(`Element with ID '${element.id}' and all nested children have been removed.`);
+    }
+}
+
+function removeDivById(divId) {
+    const element = document.getElementById(divId);
+    removeElementAndChildren(element);  // Call the recursive function
+}
+
+function deleteHelp() {
+    const elements = document.querySelectorAll('[id*="help-"]');
+    for(let element of elements) {
+        removeElementAndChildren(element);
+    }
+}
+
+function clearAllCanvases() {
+    // Find all canvas elements in the document
+    const canvases = document.querySelectorAll('canvas');
+
+    // Iterate over each canvas element
+    canvases.forEach(canvas => {
+        
+    });
+
+    console.log(`Cleared ${canvases.length} canvas elements.`);
+}
+
+function reverseMoves(moves) {
+    moves = moves.trim();
+    // Split the input string into an array of individual moves
+    let moveArray = moves.split(" ");
+    
+    // Iterate over each move and modify it
+    for (let i = 0; i < moveArray.length; i++) {
+        if (moveArray[i].includes("'")) {
+            // If the move contains a "'", remove it
+            moveArray[i] = moveArray[i].replace("'", "");
+        } else {
+            // If the move does not contain a "'", add it
+            moveArray[i] += "'";
+        }
+    }
+
+    moveArray.reverse();
+    
+    // Join the array back into a single string with space as a delimiter
+    return moveArray.join(" ");
+}
+
+function reverseXYZ(moves) {
+    moves = moves.trim();
+    // Split the input string into an array of individual moves
+    let moveArray = moves.split(" ");
+    for (let i = 0; i < moveArray.length; i++) {
+        if(moveArray[i].includes("x") || moveArray[i].includes("y") || moveArray[i].includes("z")) {
+            if (moveArray[i].includes("'")) {
+                // If the move contains a "'", remove it
+                moveArray[i] = moveArray[i].replace("'", "");
+            } else {
+                // If the move does not contain a "'", add it
+                moveArray[i] += "'";
+            }
+        }
+    }
+    return moveArray.join(" ").trim();
+}
+
+function removeXYZ(moves) {
+    moves = moves.trim();
+    // Split the input string into an array of individual moves
+    let moveArray = moves.split(" ");
+    for (let i = 0; i < moveArray.length; i++) {
+        if(moveArray[i].includes("x") || moveArray[i].includes("y") || moveArray[i].includes("z")) {
+            moveArray[i] = '';
+        }
+    }
+    return moveArray.join(" ").trim();
+}
+
+function substituteXYZ(moves) {
+    moves = moves.trim();
+    // Split the input string into an array of individual moves
+    let moveArray = moves.split(" ");
+    for (let i = 0; i < moveArray.length; i++) {
+        if(moveArray[i].includes("y")) {
+            moveArray[i] = "d U'";
+        }
+    }
+    return moveArray.join(" ").trim();
+}
+
+function countXYZ(moves) {
+    moves = moves.trim();
+    let countArray = [0,0,0,0,0,0]
+    // Split the input string into an array of individual moves
+    let moveArray = moves.split(" ");
+    for (let i = 0; i < moveArray.length; i++) {
+            let add = moveArray[i].includes("'") ? -1 : 1;
+            if(moveArray[i].includes("x")) {
+                countArray[0]+=add;
+            }
+            if(moveArray[i].includes("y")) {
+                countArray[1]+=add;
+            }
+            if(moveArray[i].includes("z")) {
+                countArray[2]+=add;
+            }
+            if(moveArray[i].includes("r")) {
+                countArray[3]+=add;
+            }
+            if(moveArray[i].includes("f")) {
+                countArray[4]+=add;
+            }
+            if(moveArray[i].includes("u")) {
+                countArray[5]+=add;
+            }
+    }
+    countArray[0] = countArray[0] % 4; //x
+    countArray[1] = countArray[1] % 4; //y
+    countArray[2] = countArray[2] % 4; //z
+    countArray[3] = countArray[3] % 4; //r
+    countArray[4] = countArray[4] % 4; //f
+    countArray[5] = countArray[5] % 4; //u
+    return countArray;
+}
+
+function preventXYZ(countArray) {
+    let preventXYZstring = "";
+    let reverseBoolean = false;
+    let cI = 0;
+    
+    //x
+    cI = 0;
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "r' L ";
+        } else {
+            preventXYZstring += "r L' ";
+        }
+        
+    }
+
+    //y
+    cI = 1
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "d' U ";
+        } else {
+            preventXYZstring += "d U' ";
+        }
+        
+    }
+
+    //z
+    cI = 2
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "f B' ";
+        } else {
+            preventXYZstring += "f' B ";
+        }
+        
+    }
+
+    //r
+    cI = 3;
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "r' L ";
+        } else {
+            preventXYZstring += "r L' ";
+        }
+        
+    }
+
+    //f
+    cI = 4;
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "d' U ";
+        } else {
+            preventXYZstring += "d U' ";
+        }
+        
+    }
+
+    //u
+    cI = 5;
+    reverseBoolean = countArray[cI] < 0; 
+    countArray[cI] = Math.abs(countArray[cI]);
+    for(let i = 0; i < countArray[cI]; i++) {
+        if(reverseBoolean)  {
+            preventXYZstring += "f B' ";
+        } else {
+            preventXYZstring += "f' B ";
+        }
+        
+    }
+    return preventXYZstring;
+}
+
+function replaceXYZRUF(input) {
+    let newInput = input
+    .replaceAll("x'","r' L")
+    .replaceAll("y'","d' U ")
+    .replaceAll("z'","")
+    .replaceAll("r'","")
+    .replaceAll("f'","")
+    .replaceAll("u'","")
+
+    .replaceAll("x","r L' ")
+    .replaceAll("y","d U' ")
+    .replaceAll("z","")
+    .replaceAll("r","")
+    .replaceAll("f","")
+    .replaceAll("u","");
+    return newInput;
+}
+
+'FRU','FRD',
+'RBU','RBD',
+'BLU','BLD',
+'FLU','FLD'
+
+let currentNum = 0;
+let currentMove = 0;
+
+let currentNumArray = [0,0,0,0];
+let currentMoveArray = [0,0,0,0];
+
+testAlg = algoArrayPrime[0][0][1];
+partialSolve = "";
+setTimeout(()=>{
+    initialButtonTracker();
+    loadCurrent(0,200,0,true);
+    loadCurrent(1,200,0,true);
+    loadCurrent(2,200,0,true);
+    loadCurrent(3,200,0,true);
+},500)
+
+
+setTimeout(deleteHelp, 4);
+
+
+function nextAction(arrayIndex=0) {
+    let currentNum = currentNumArray[arrayIndex];
+    let currentMove = currentMoveArray[arrayIndex];
+    let moves = algoArrayPrime[arrayIndex][currentNum][1].split(' ');
+    if(currentMove > moves.length-1) {
+        currentNumArray[arrayIndex] = currentNum + 1;
+        currentMoveArray[arrayIndex] = 0;
+        testAlg = algoArrayPrime[arrayIndex][currentNum][1];
+        partialSolve = concatAlgoArrayUpTo(currentNumArray[arrayIndex],algoArrayPrime[arrayIndex]);
+        loadCurrent(arrayIndex,100,currentNumArray[arrayIndex],true);
+    } else {
+        pressNextButton(arrayIndex);
+        currentMoveArray[arrayIndex] = currentMove + 1;
+    }
+    deleteHelp();
+}
+
+function pressNextButton(arrayIndex=0,id="") {
+    if(id=="") {
+
+        const elements = document.querySelectorAll('[id*="next-"]');
+        for(let element of elements) {
+            if(buttonTracker[arrayIndex].includes(element)) {
+                element.click();
+            }
+        }
+        //elements[0].click();
+    } else {
+        document.getElementById(id).click();
+    }
+}
+
+function initialButtonTracker() {
+    const elements = document.querySelectorAll('[id*="next-"]');
+    for(let i=0;i<elements.length;i++) {
+        if(i<2) {
+            buttonTracker[0].push(elements[i]);
+        } else if(i<4) {
+            buttonTracker[1].push(elements[i]);
+        } else if(i<6) {
+            buttonTracker[2].push(elements[i]);
+        } else if(i<8) {
+            buttonTracker[3].push(elements[i]);
+        } else {
+
+        }
+    }
+    console.log(buttonTracker);
+}
+
+function updateButtonTracker(arrayIndex=0) {
+    let oldButtonTracker = buttonTracker[arrayIndex];
+    let newButtonTracker = [];
+    const elements = document.querySelectorAll('[id*="next-"]');
+    console.log(elements)
+    for(let element of elements) {
+        if(!buttonTracker.flat().includes(element)) {
+            newButtonTracker.push(element);
+        }
+    }
+    console.log(newButtonTracker)
+    buttonTracker[arrayIndex] = newButtonTracker;
+}
+
+function concatAlgoArrayUpTo(number,array=algoArrayPrime[0]) {
+    let string = ""
+    for(let i = 0; i < number; i++) {
+        string += array[i][1];
+        string += " "
+    }
+    return string.trim();
+}
+
+
+
+document.getElementById('b1').addEventListener('click', function() {
+    nextAction(0);
+});
+
+
+document.getElementById('b2').addEventListener('click', function() {
+    nextAction(1);
+});
+
+document.getElementById('b3').addEventListener('click', function() {
+    nextAction(2);
+});
+
+document.getElementById('b4').addEventListener('click', function() {
+    nextAction(3);
+});
+
+document.getElementById('b5').addEventListener('click', function() {
+    nextAction(0);
+    nextAction(1);
+    nextAction(2);
+    nextAction(3);
+});
+
+
+
+
+
